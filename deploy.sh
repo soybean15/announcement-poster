94bb8e1787cbaab42bb74bc60f035ca7ee04e15f
soybean15 <marlonpadilla1593@gmail.com>
2023-11-16 22:33:24 +0800

    updated size

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
- git push -f https://github.com/soybean15/announcement-poster.git git a:gh-pages
+ git push -f https://github.com/soybean15/announcement-poster.git  master:gh-pages
 
 cd -
