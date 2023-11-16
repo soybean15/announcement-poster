5e63653b7104e4ccf8601f94a049fe40db3c07e1
soybean15 <marlonpadilla1593@gmail.com>
2023-11-16 22:15:32 +0800

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  <home-view/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import HomeView from './views/HomeView.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HomeView
   }
 }
 </script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,32 @@
+<template>
+ <div class="m-10 items-start relative py-5 flex lg:flex-row flex-col md:items-center border rounded-md shadow-md">
+  <!-- Your content goes here -->
+
+
+
+    <div class=" w-full mr-5">
+        <img src="@/assets/planting.png"/>
+    </div>
+  
+    
+    <div class="text-5xl my-10  px-10 text-left text-green-900 font-bebas ">
+       <p> "The Best time to plant a tree was twenty years ago, the second best is now"</p>
+       <p class="text-sm font-sans  text-green-800"> Join the movement for a sustainable future—let's collectively make a difference by planting trees today, nurturing the environment for generations to come.
+ </p>
+    </div>
+
+    <div class="absolute bottom-5 right-5 text-green-900">
+        www.ecosia.org
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
