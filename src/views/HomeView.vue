d5bc1cd04d0672846831fee75822f7538e85d3f4
soybean15 <marlonpadilla1593@gmail.com>
2023-11-16 22:52:45 +0800

    updated size

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,15 @@
 <template>
- <div class="m-10 items-start  relative py-5 flex lg:flex-row flex-col md:items-center border rounded-md shadow-md">
+ <div class="m-5 sm:m-10 items-start  relative py-5 flex lg:flex-row flex-col md:items-center border rounded-md shadow-md">
   <!-- Your content goes here -->
 
 
 
     <div class=" w-max mr-5">
-        <img class=" w-[30rem] lg:w-[100rem] sm:w-[55rem]"  src="@/assets/planting.png" />
+        <img class=" w-[20rem] lg:w-[100rem] sm:w-[55rem]"  src="@/assets/planting.png" />
     </div>
   
     
-    <div class="text-7xl my-10  px-10 text-left text-green-900 font-bebas ">
+    <div class="text-4xl sm:text-7xl my-10  px-10 text-left text-green-900 font-bebas ">
        <p> "The Best time to plant a tree was twenty years ago, the second best is now"</p>
        <p class="text-sm font-sans  text-green-800"> Join the movement for a sustainable future—let's collectively make a difference by planting trees today, nurturing the environment for generations to come.
  </p>
