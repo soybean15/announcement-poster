0f2348398f0e6457047855290bcb826868b758e6
soybean15 <marlonpadilla1593@gmail.com>
2023-11-17 20:12:51 +0800

    test

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === 'production'
-  ? '/announcement-poster/'
-  : '/',
-  outputDir: 'dist',
+  // publicPath: process.env.NODE_ENV === 'production'
+  // ? '/announcement-poster/'
+  // : '/',
   transpileDependencies: true
 })
